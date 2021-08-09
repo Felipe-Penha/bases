0209af927b243ee1d315da48ee15d5dbcd54b449
Felipe Penha <ftspenha@gmail.com>
2021-08-09 08:36:21 -0300

    correcting group images

--- pages/components/Group.js
+++ pages/components/Group.js
@@ -9,17 +9,32 @@ const Group = () => {
       <h2 className={`group-title`}>Grupo</h2>
       <div className={`group-cards`}>
         <div className={`group-card`}>
-          <Image className={`group-card-img`} src={arthur} />
+          <Image
+            className={`group-card-img`}
+            src={arthur}
+            width={100}
+            height={100}
+          />
           <h2 className={`group-card-text`}>Arthur Ferreira</h2>
         </div>
 
         <div className={`group-card`}>
-          <Image className={`group-card-img`} src={natasha} />
+          <Image
+            className={`group-card-img`}
+            src={natasha}
+            width={100}
+            height={100}
+          />
           <h2 className={`group-card-text`}>Natasha Oliveira</h2>
         </div>
 
         <div className={`group-card`}>
-          <Image className={`group-card-img`} src={leonardo} />
+          <Image
+            className={`group-card-img`}
+            src={leonardo}
+            width={100}
+            height={100}
+          />
           <h2 className={`group-card-text`}>Leonardo Dalbem</h2>
         </div>
       </div>
