e6ca3fa29039fb8c938f8799cc09c71d6808d041
Felipe Penha <ftspenha@gmail.com>
2021-08-09 14:48:52 -0300

    putting animations and navbar and footer links

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -1,53 +1,84 @@
+import Link from 'next/link';
+
 const Footer = () => {
   return (
     <div className={`footer`}>
       <navbar className={`footer-bar`}>
-        <div className={`footer-bar-if-logo`}>
-          <svg
-            width='45'
-            height='62'
-            viewBox='0 0 45 62'
-            fill='none'
-            xmlns='http://www.w3.org/2000/svg'
-          >
-            <rect x='15.7836' width='13.4328' height='13.4328' fill='#88D066' />
-            <rect
-              x='31.5672'
-              y='32.2388'
-              width='13.4328'
-              height='13.4328'
-              fill='#88D066'
-            />
-            <rect x='31.5672' width='13.4328' height='13.4328' fill='#88D066' />
-            <rect
-              x='15.7836'
-              y='48.3582'
-              width='13.4328'
-              height='13.4328'
-              fill='#88D066'
-            />
-            <rect
-              x='15.7836'
-              y='32.2388'
-              width='13.4328'
-              height='13.4328'
-              fill='#88D066'
-            />
-            <rect
-              x='15.7836'
-              y='16.1194'
-              width='13.4328'
-              height='13.4328'
-              fill='#88D066'
-            />
-            <rect y='48.3582' width='13.4328' height='13.4328' fill='#88D066' />
-            <rect y='32.2388' width='13.4328' height='13.4328' fill='#88D066' />
-            <rect y='16.1194' width='13.4328' height='13.4328' fill='#88D066' />
-            <circle cx='6.71642' cy='6.71642' r='6.71642' fill='#E65555' />
-          </svg>
-        </div>
+        <Link href='/'>
+          <a>
+            <div className={`footer-bar-if-logo`}>
+              <svg
+                width='45'
+                height='62'
+                viewBox='0 0 45 62'
+                fill='none'
+                xmlns='http://www.w3.org/2000/svg'
+              >
+                <rect
+                  x='15.7836'
+                  width='13.4328'
+                  height='13.4328'
+                  fill='#88D066'
+                />
+                <rect
+                  x='31.5672'
+                  y='32.2388'
+                  width='13.4328'
+                  height='13.4328'
+                  fill='#88D066'
+                />
+                <rect
+                  x='31.5672'
+                  width='13.4328'
+                  height='13.4328'
+                  fill='#88D066'
+                />
+                <rect
+                  x='15.7836'
+                  y='48.3582'
+                  width='13.4328'
+                  height='13.4328'
+                  fill='#88D066'
+                />
+                <rect
+                  x='15.7836'
+                  y='32.2388'
+                  width='13.4328'
+                  height='13.4328'
+                  fill='#88D066'
+                />
+                <rect
+                  x='15.7836'
+                  y='16.1194'
+                  width='13.4328'
+                  height='13.4328'
+                  fill='#88D066'
+                />
+                <rect
+                  y='48.3582'
+                  width='13.4328'
+                  height='13.4328'
+                  fill='#88D066'
+                />
+                <rect
+                  y='32.2388'
+                  width='13.4328'
+                  height='13.4328'
+                  fill='#88D066'
+                />
+                <rect
+                  y='16.1194'
+                  width='13.4328'
+                  height='13.4328'
+                  fill='#88D066'
+                />
+                <circle cx='6.71642' cy='6.71642' r='6.71642' fill='#E65555' />
+              </svg>
+            </div>
 
-        <h2 className={`footer-bar-title`}>Concepções da Educação</h2>
+            <h2 className={`footer-bar-title`}>Concepções da Educação</h2>
+          </a>
+        </Link>
       </navbar>
     </div>
   );
