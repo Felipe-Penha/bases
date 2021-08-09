82e336c21ddcbc4cd0fdd723470bf23d01426a1e
Felipe Penha <ftspenha@gmail.com>
2021-08-09 08:35:24 -0300

    correcting group images

--- pages/components/Group.js
+++ pages/components/Group.js
@@ -1,20 +1,25 @@
+import Image from 'next/image';
+import arthur from '../../public/arthur.png';
+import natasha from '../../public/natasha.png';
+import leonardo from '../../public/leonardo.png';
+
 const Group = () => {
   return (
     <div className={`group`}>
       <h2 className={`group-title`}>Grupo</h2>
       <div className={`group-cards`}>
         <div className={`group-card`}>
-          <img className={`group-card-img`} src='arthur.png' />
+          <Image className={`group-card-img`} src={arthur} />
           <h2 className={`group-card-text`}>Arthur Ferreira</h2>
         </div>
 
         <div className={`group-card`}>
-          <img className={`group-card-img`} src='natasha.png' />
+          <Image className={`group-card-img`} src={natasha} />
           <h2 className={`group-card-text`}>Natasha Oliveira</h2>
         </div>
 
         <div className={`group-card`}>
-          <img className={`group-card-img`} src='leonardo.png' />
+          <Image className={`group-card-img`} src={leonardo} />
           <h2 className={`group-card-text`}>Leonardo Dalbem</h2>
         </div>
       </div>
